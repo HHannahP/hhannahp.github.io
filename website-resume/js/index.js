a2fc32339da83af3b8d59028e82b46694810cc09
HHannahP <hass.hannah30@gmail.com>
2016-03-18 21:07:00 -0700

    Added files via upload

---
+++ website-resume/js/index.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+    $('.resume').funnyText({
+        speed: 700,
+        borderColor: 'black',
+        activeColor: 'blue',
+        color: 'purple',
+        fontSize: '2em',
+        direction: 'both'
+    });
+});
