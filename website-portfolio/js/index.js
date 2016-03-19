12962b5f8fb9f6315e2affd2a73c06e5a9a2105f
HHannahP <hass.hannah30@gmail.com>
2016-03-18 21:06:23 -0700

    Added files via upload

---
+++ website-portfolio/js/index.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+    $('.portfolio').funnyText({
+        speed: 700,
+        borderColor: 'black',
+        activeColor: 'blue',
+        color: 'darkgreen',
+        fontSize: '2em',
+        direction: 'both'
+    });
+});
